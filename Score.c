6ee87adbe9cec4e7e86618e9e5cca2d3ad5839e9
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2023-11-08 23:38:45 +0900

    코드 추가: BAD의 경우 life --

--- Score.c
+++ Score.c
@@ -2,11 +2,12 @@
 
 int main()
 {
+        life=1;
 	char n;
 	int sum = 0;
 	if (n == "Perfect") sum += 30;
 	else if (n == "Good") sum += 10;
-	else if (n == "Bad") sum += 0;
+	else if (n == "Bad"){ sum += 0; life--;}
 
   printf("Score = %d", sum);
 }
