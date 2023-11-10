fc41bac29ab3aab60c8e27f724eae129e2408476
coco <136443156+CoCo-1223@users.noreply.github.com>
2023-11-11 08:59:55 +0900

    Update Score.c
    스코어 판정하는 경우 수정

--- Score.c
+++ Score.c
@@ -1,13 +1,23 @@
-#include<stdio.h>
-
-int main()
+int score()
 {
-        life=1;
-	char n;
+	int life = 1;
+	int n;
 	int sum = 0;
-	if (n == "Perfect") sum += 30;
-	else if (n == "Good") sum += 10;
-	else if (n == "Bad"){ sum += 0; life--;}
+	//Bad일 경우
+	if (n == 0) {
+		sum += 0; life--;
+	}
+	//perfect일 경우
+	else if (n >= -0.1 && n <= 0.1) {
+		sum += 30;
+	}
+	//Good일 경우
+	else if (n >= -0.5 && n <= 0.5) {
+		sum += 10;
+	}
+	//생명이 '0'미만이 된 경우
+	if (life < 0) return 0;
 
-  printf("Score = %d", sum);
+	//점수출력
+	printf("Score = %d", sum);
 }
