0c520f3068f3bd6421bfb12dc0802dcbc62b97dd
coco <136443156+CoCo-1223@users.noreply.github.com>
2023-11-11 09:05:54 +0900

    Update Score.c
    인자 스코어로 받아야겠다

--- Score.c
+++ Score.c
@@ -1,23 +1,22 @@
-int score()
+int score(int score)
 {
 	int life = 1;
-	int n;
 	int sum = 0;
 	//Bad일 경우
-	if (n == 0) {
+	if (score == 0) {
 		sum += 0; life--;
 	}
 	//perfect일 경우
-	else if (n >= -0.1 && n <= 0.1) {
+	else if (score >= -0.1 && score <= 0.1) {
 		sum += 30;
 	}
 	//Good일 경우
-	else if (n >= -0.5 && n <= 0.5) {
+	else if (score >= -0.5 && score <= 0.5) {
 		sum += 10;
 	}
 	//생명이 '0'미만이 된 경우
 	if (life < 0) return 0;
 
 	//점수출력
-	printf("Score = %d", sum);
+	printf("Score = %d", score);
 }
