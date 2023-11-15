6a4c640673889a203bc128e267460ae27ca10917
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2023-11-15 21:34:34 +0900

    코드 수정: 조건문 수정

--- Score.c
+++ Score.c
@@ -1,22 +1,31 @@
+int sum = 0;
+int life = 1;
 int score(int score)
 {
-	int life = 1;
-	int sum = 0;
-	//Bad일 경우
-	if (score == 0) {
-		sum += 0; life--;
-	}
+	
 	//perfect일 경우
-	else if (score >= -0.1 && score <= 0.1) {
+	if (score==1) {
+		printf("Perfect!");
 		sum += 30;
 	}
 	//Good일 경우
-	else if (score >= -0.5 && score <= 0.5) {
+	else if (score==2) {
+		printf("Good!");
 		sum += 10;
 	}
+	//Bad일 경우
+	else if (score == 3) {
+		printf("Miss..");
+		sum += 0;
+		life--;
+	}
+
 	//생명이 '0'미만이 된 경우
-	if (life < 0) return 0;
+	if (life <0) {
+		printf("Game over");
+		return 0;
+	}
 
 	//점수출력
-	printf("Score = %d", score);
+	printf("Score = %d\n", sum);
 }
