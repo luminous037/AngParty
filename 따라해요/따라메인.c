49eea15c7f1470e7bc94788d0b170997a335b39b
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2023-11-21 14:04:07 +0900

    Update 따라메인.c

--- 따라해요/따라메인.c
+++ 따라해요/따라메인.c
@@ -3,11 +3,24 @@
 #include <Windows.h>
 #include <mmsystem.h>
 #include <conio.h>
+#include <Digitalv.h>
 #include <time.h>
+
+// 탁 탁 간격 0.8초
+//첫 시작 1초 뒤
+//탁탁 탁 탁 탁
+//0.37 0.8 0.6 0.6
+//2.9
+
+//#define BGM "따라해요 본겜.wav"
+//#define SHUFFLE1 "북.wav"    //효과음 경로 지정
+//#define SHUFFLE2 "챙.wav"    //효과음 경로 지정
 
 #pragma comment(lib,"winmm.lib")
 #define SPACE 32 //스페이스 키 값
 #define ENTER 13//엔터 키 값
+
+
 
 void Play_Music() {
 	PlaySound(TEXT("따라해요 본겜.wav"), NULL, SND_ASYNC);  //노래 재생
@@ -21,10 +34,32 @@ int Key_input(int n) {  //입력 받은 키
 		}
 	}
 
+
+//MCI_OPEN_PARMS openBgm;
+//MCI_PLAY_PARMS playBgm;
+//MCI_OPEN_PARMS openShuffleSound;
+//MCI_PLAY_PARMS playShuffleSound;
+//
+//int dwID;
+//
+//void playingBgm(void) {
+//	openBgm.lpstrElementName = "따라해요 본겜.wav";            //파일 오픈
+//	mciSendCommand(0, MCI_OPEN, MCI_OPEN_ELEMENT | MCI_OPEN_TYPE, (DWORD)(LPVOID)&openBgm);
+//	dwID = openBgm.wDeviceID;
+//	mciSendCommand(dwID, MCI_PLAY, MCI_NOTIFY, (DWORD)(LPVOID)&openBgm);    //음악 한 번 재생
+//}
+//void playingShuffleSound(void) {
+//	openShuffleSound.lpstrElementName = SHUFFLE1;    //파일 오픈
+//	mciSendCommand(0, MCI_OPEN, MCI_OPEN_ELEMENT | MCI_OPEN_TYPE, (DWORD)(LPVOID)&openShuffleSound);
+//	dwID = openShuffleSound.wDeviceID;
+//	mciSendCommand(dwID, MCI_PLAY, MCI_NOTIFY, (DWORD)(LPVOID)&openShuffleSound);    //음악을 한 번 재생
+//	Sleep(1800);    //효과음이 재생될 때까지 정지했다가
+//	mciSendCommand(dwID, MCI_SEEK, MCI_SEEK_TO_START, (DWORD)(LPVOID)NULL);    //음원 재생 위치를 처음으로 초기화
+//}
 int sum = 0;
 int life = 1;
-float t[10]; //입력된 북or챙 출력된 시간 저장
-int order[10];
+float t[10]; // 입력된 북or챙 출력된 시간 저장
+int order[10]; //순서 저장
 
 int score(int score)
 {
@@ -42,7 +77,6 @@ int score(int score)
 	//Bad일 경우
 	else if (score == 3) {
 		printf("Miss..");
-		sum += 0;
 		life--;
 	}
 
@@ -56,29 +90,28 @@ int score(int score)
 	printf("Score = %d\n", sum);
 }
 
+
 void judge(float t) { //판정 (현재 임의로 정함)
-	if (t > 2.8 && t < 3) {
+	if (t >= 2.8 && t <= 3.2) {
 		score(1);
-	}
-	else if ((t >= 2.3 && t <= 2.8) && (t >= 3 && t <= 3.5)) {
-		score(2);
 	}
 	else {
 		score(3);
 	}
 }
 
+
 void SpaceKey() {
-	//PlaySound(TEXT("챙.wav"), NULL, SND_ASYNC);  //노래 재생 싱크 안맞음
-	printf("챙\n");
+	PlaySound(TEXT("챙.wav"), NULL, SND_ASYNC);  //노래 재생 싱크 안맞음
+	printf(" 챙\n");
 }
 
 void EnterKey() {
-	//PlaySound(TEXT("북.wav"), NULL, SND_ASYNC);
-	printf("북\n"); //Beep(300, 200);
+	PlaySound(TEXT("북.wav"), NULL, SND_ASYNC);
+	printf(" 북\n"); //Beep(300, 200);
 }
 
-void route1(){ //4박자
+int route1(){ //4박자
 	int num;
 	int index = 0;
 	for (int i = 0; i < 4; i++) {
@@ -98,10 +131,23 @@ void route1(){ //4박자
 		if (i == 3) break;
 		else Sleep(780); //탁 -> 탁 사이 시간
 	}
-
+	float input_time;
+	for (int i = 0; i < index; i++) {
+		if (time(NULL) - t[index] == 3.5) return;
+		if (time(NULL) - t[i] > 0.75 * 4 + 0.2) {
+			i++;
+		}
+		if (_getch() == order[i]) {
+			input_time = time(NULL) - t[i];
+			judge(input_time);
+			i++;
+		}
+		else score(3);
+	}
+	return;
 }
 
-void route2() { //5박자
+int route2() { //5박자
 	int num;
 	int index = 0;
 	for (int i = 0; i < 5; i++) {
@@ -119,50 +165,59 @@ void route2() { //5박자
 		}
 		index++;
 		if (i == 0) Sleep(370);
-		else if (i == 4) Sleep(900);
+		else if (i == 4) {
+			Sleep(900);
+			return index;
+		}
 		else Sleep(520);
 	}
+	/*float input_time; //판정 아직 못함
+	for (int i = 0; i < index; i++) {
+		if (time(NULL) - t[index] == 3.5) return;
+		if (time(NULL) - t[i] > 0.75 * 4 + 0.2) {
+			i++;
+		}
+		if (_getch() == order[i]) {
+			input_time = time(NULL) - t[i];
+			judge(input_time);
+			i++;
+		}
+		else score(3);
+	}
+	return;*/
 }
 
-void PlayerKey() {
-	int n;
-	int num;
-	int index = 0;
-	for (int i = 0; i < 4;i++) {
-		if (time(NULL) - t[index] > 3) printf("GameOver");
-		n = Key_input(_kbhit());
-
-		if (n == order[index]) {
-			SpaceKey();
-			judge(time(NULL) - t[index]);
-			index++;
+void PlayerKey(int n) {
+	float input_time;
+	for (int i = 0; i < n; i++) {
+		if (time(NULL) - t[n] == 3.5) return;
+		if (time(NULL) - t[i] > 0.75 * 4+0.2) {
+			i++;
 		}
-
-		else if (n == order[index]) {
-			EnterKey();
-			judge(time(NULL) - t[index]);
-			index++;
+		if (_kbhit() == order[i]) {
+			input_time = time(NULL) - t[i];
+			judge(input_time);
+			i++;
 		}
-
-		else {
-			life--;
-			if (life < 0) printf("Gameover");
-		}
+		else score(3);
 	}
+	return;
 }
 
 void Showbit() {
+	int n, m;
 	for (int i = 0; i < 4; i++) {
 		if(i==0) Sleep(1500);
-		route1();
-		//PlayerKey();
+		n=route1();
+		//PlayerKey(n);
 		Sleep(3500);
-		route2();
-		//PlayerKey();
+		m=route2();
+		//PlayerKey(m);
 		Sleep(2850);
 	}
 	return;
 }
+
 
 int main() {
 	Play_Music();
@@ -171,7 +226,7 @@ int main() {
 	int index = 0;
 	Showbit();
 	//t = 5.2;
-	while (1) {
+	/*while (1) {
 		n = Key_input(kbhit());
 
 		if (n == 1) {
@@ -192,6 +247,6 @@ int main() {
 			printf("%d",n-t[index]);
 			return 0;
 		}
-	}
+	}*/
 	return 0;
 }
