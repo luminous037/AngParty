701dcc56a4bf6e5b41380eb3ab79aae15f018cd0
coco <136443156+CoCo-1223@users.noreply.github.com>
2023-11-23 01:33:06 +0900

    Update dropNote.c
    각 열에서는 출력되는데 어떻게 처음으로 돌아가서 다시내리지。。

--- 손가락댄스파티/dropNote.c
+++ 손가락댄스파티/dropNote.c
@@ -5,10 +5,11 @@
 
 #include "Gotoxy.h"
 
-
-
 #define width 230
-#define height 75
+#define height 80
+
+int ArrowType;
+COORD pos;
 
 void setConsoleSize() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -20,70 +21,91 @@ void setConsoleSize() {
 	SetConsoleScreenBufferSize(hConsole, bufferSize);
 }
 
-void Beat_down() {
-	int i = 0;
-	while (1) {
-		gotoxy(0, 0);
-		printf("\n");
-		i++;
-		Sleep(100);
-		if (i == 20) {
-			return;
-		}
-	}
-}
+// void print_frame(HANDLE handle)
+// {
+// 	int i;
+// 
+// 	pos.X = 1;
+// 	pos.Y = 1;
+// 	SetConsoleCursorPosition(handle, pos);
+// 	printf("\u250D\u2501\u2501\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2501\u2501\u2511");
+// 
+// 	for (i = 2; i < 25; i++)
+// 	{
+// 		pos.X = 1;
+// 		pos.Y = i;
+// 		SetConsoleCursorPosition(handle, pos);
+// 		printf("\u2502          \u2502          \u2502          \u2502          \u2502");
+// 	}
+// 
+// 	pos.X = 1;
+// 	pos.Y = 25;
+// 	SetConsoleCursorPosition(handle, pos);
+// 	printf("\u251D\u2501\u2501\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u2501\u2501\u2525");
+// 
+// 	for (i = 26; i <= 27; i++)
+// 	{
+// 		pos.X = 1;
+// 		pos.Y = i;
+// 		SetConsoleCursorPosition(handle, pos);
+// 		printf("\u2502          \u2502          \u2502          \u2502          \u2502");
+// 	}
+// 
+// 	pos.X = 1;
+// 	pos.Y = 28;
+// 	SetConsoleCursorPosition(handle, pos);
+// 	printf("\u2515\u2501\u2501\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2501\u2501\u2519");
+// }
 
-void printNote(char note, int yPos) {
-	for (int i = 0; i < yPos; ++i) {
-		printf("\n");
-	}
-	printf(" %c \n", note);
-}
+// void downNote(int yPos) {
+// 	for (int i = 0; i < yPos; ++i) {
+// 		printf("\n");
+// 	}
+// }
 
 int main() {
-	int yPos = 0;
 	setConsoleSize();
 	//CursorView(0);
 	int n = 4, i = 0;
+	//void print_frame(handle);
 	int key_max;
 	int random;
 
-	
-	for (i = 0; i < 10; i++) {
-	
-		key_max = rand() % 2;  //화살표 한 개 출력 or 두개 출력 여부 확인]
-		for (int j = 0; j < key_max + 1; j++) {
+	while (1) {
+		for (i = 0; i < height; i++) {
+			//system("cls");
+			key_max = rand() % 2;  //화살표 한 개 출력 or 두개 출력 여부 확인]
+			
+			for (int j = 0; j < key_max + 1; j++) {
+				random = rand() % 4; // 출력할 화살표 모양을 정함
+				srand(time(NULL));
+				
+				if (random == n) break; //화살표를 두 번 출력할 경우, 만약 그 두개의 화살표가 같은 모양이면 두번째 화살표는 출력하지 않고 break;
 
-			random = rand() % 4; // 출력할 화살표 모양을 정함
-			srand(time(NULL));
-
-			if (random == n) break; //화살표를 두 번 출력할 경우, 만약 그 두개의 화살표가 같은 모양이면 두번째 화살표는 출력하지 않고 break;
-
-			n = random;
-
-			if (n == 0) {  //위 화살표
-				gotoxy(10, 5);
-				printf(" up "); //화살표 모양 있어야함
+				n = random;
+				
+				if (n == 0) {  //위 화살표
+					gotoxy(10, i % height); // i % height로 y120에 들어가면 0
+					printf("up "); //화살표 모양 있어야함
+					printf("\n");
+				}
+				else if (n == 1) {  //아래 화살표
+					gotoxy(20, i % height);
+					printf("down ");
+				}
+				else if (n == 2) {  // 오른쪽 화살표
+					gotoxy(30, i % height);
+					printf("right ");
+				}
+				else if (n == 3) {  //왼쪽 화살표
+					gotoxy(40, i % height);
+					printf("left ");
+				}
 			}
-			else if (n == 1) {  //아래 화살표
-				gotoxy(20, 5);
-				printf("down ");
-			}
-			else if (n == 2) {  // 오른쪽 화살표
-			gotoxy(30, 5);
-				printf("right ");
-			}
-			else if (n == 3) {  //왼쪽 화살표
-				gotoxy(40, 5);
-				printf("left ");
-			}
+			n = 4; //n은 4로 꼭 초기화
+			Sleep(500); //0.5초 뒤 이어서 출력
+			printf("\n");
 		}
-		gotoxy(0, 0);
-		//printf("\n");
-		//Beat_down();
-		n = 4; //n은 4로 꼭 초기화
-		Sleep(500); //0.5초 뒤 이어서 출력
-		printf("\n");
 	}
 	return 0;
 }
