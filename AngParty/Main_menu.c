51d20290d4e8beb42b7871298166a4f4ca5e5c57
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2023-11-24 18:31:44 +0900

    코드 수정: 따라해요 앙버터 실행

--- AngParty/Main_menu.c
+++ AngParty/Main_menu.c
@@ -16,6 +16,8 @@
 
 #define MAGIC_KEY 224 
 #define SPACE 32 
+#define ENTER 13
+#define ESC 27
 
 
 
@@ -108,29 +110,30 @@ void main_menu() {
 	while (1) //키보드 움직임
 	{
 		int key;
+		int num=1;
 		if (_kbhit())
 		{
 			key = _getch();
 
-			if (key == 27) {
+			if (key == ESC) {
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 				gotoxy(0, 0);
-
+				
 
 				SetScreen();
-				int n = exitscreen();
+				int n=exitscreen();
 				if (n == 1) {
-					PlaySound(TEXT("gameover.wav"), NULL, SND_ASYNC | SND_ASYNC);
+					PlaySound(TEXT("gameover.wav"), NULL, SND_ASYNC | SND_LOOP);
 					gotoxy(0, 0);
 					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0);
 					SetScreen();
 					Sleep(1300);
 					return 1;
 				}
-				else if (n == 2) {
+				else if(n==2){
 					gotoxy(0, 0);
 					return 2;
-				}
+			}
 
 			}
 
@@ -141,7 +144,7 @@ void main_menu() {
 				{
 				case 72: //상
 					PlaySound(TEXT("followmestart.wav"), NULL, SND_ASYNC | SND_LOOP);
-
+					num = 1;
 
 					gotoxy(17, 45);
 
@@ -172,7 +175,8 @@ void main_menu() {
 				case 80: //하
 
 					PlaySound(TEXT("fingerparty.wav"), NULL, SND_ASYNC | SND_LOOP);
-
+					num = 2;
+	
 					gotoxy(17, 28);
 					printf("\n");
 					printf("  \n");
@@ -203,7 +207,9 @@ void main_menu() {
 
 
 			}
-
+			if (key == ENTER) {
+				Ang1();
+			}
 		}
 	}
 }
