334c3ff273152845911119673d37c550ffcfb05a
Ji-ji <135002121+luminous037@users.noreply.github.com>
2023-11-29 16:32:57 +0900

    main에서 따라해요게임 합침

--- AngParty/main.c
+++ AngParty/main.c
@@ -84,6 +84,22 @@ int main() {
 
     else if (n == 2) main();
 
+    else if (n == 3) {
+        gotoxy(0, 0);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        SetScreen();
+        followmeGame();
+    }
+
+    else if (n == 4) {
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        gotoxy(0, 0);
+        SetScreen();
+        gotoxy(0, 1);
+        printf("앙냥이의 손가락파티를 선택하셨네요?");
+    }
+
     return 0;
 
 }
