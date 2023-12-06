27f8ad009983ffed74499256a4d0e5ebd7e0db25
GAUDUL <137772044+GAUDUL@users.noreply.github.com>
2023-12-06 12:16:46 +0900

    Update Gotoxy.h

--- AngParty/Gotoxy.h
+++ AngParty/Gotoxy.h
@@ -18,21 +18,4 @@ void gotoxy(int x, int y) //커서 이동 함수 (가우둘 코드 참고)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-
-enum KEYBOARD { //열거형, 명명된 값의 집합, 고정된 상수들의 집합 (가우둘 코드참고)
-    UP = 72, LEFT = 75, RIGHT = 77, DOWN = 80
-};
-
-int Key_input(int n) {  //입력 받은 방향키 (가우둘)
-    if (n == MAGIC_KEY) { //(스페이스바 입력)
-        switch (getch()) {
-        case UP: return 1;
-        case DOWN: return 2;
-        case RIGHT:return 3;
-        case LEFT: return 4;
-        }
-    }
-    return 0;
-}
-
 #endif
